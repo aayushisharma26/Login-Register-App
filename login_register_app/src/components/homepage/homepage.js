83256a09275e57a62c508dacfb29c326db41e7b8
Aayushi Sharma <aayushisharma20@navgurukul.org>
2023-08-27 23:26:56 +0530

    Create-Login-Register

---
+++ login_register_app/src/components/homepage/homepage.js
@@ -0,0 +1,11 @@
+import React from "react"
+import "./homepage.css"
+
+const Homepage =()=>{
+    return (
+        <div className="homepage">
+            <div className="button">Logout</div>
+        </div>
+    )
+}
+export default Homepage
